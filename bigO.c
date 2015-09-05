0a5615369a9a9807437248f6dcd924b014106674
Amar Shah <amar@shareprogress.org>
2015-09-05 02:03:24 -0400

    Add answers to VI.7 and 8

--- bigO.c
+++ bigO.c
@@ -78,3 +78,19 @@ int sqrt2(int n) {
   }
   return -1;
 }
+
+// VI.7, p. 57
+// If a binary search tree is not balanced, how long might it take (worst case)
+// to fins an element in it?
+//
+// Answer: O(n). If the BST is completely unbalanced, then it will either have
+// no nodes with left-children or no nodes with right-children. The only leaf
+// node will be either the maximum or minimum element, and finding this element
+// will require searching through all the other nodes first.
+
+// VI.8, p. 57
+// You are looking for a specific value in a binary tree, but the tree is not
+// a binary search tree. What is the time complexity of this?
+//
+// Answer: O(n). There's no ordering so you may have to look at all the nodes
+// before you find the target.
