df694dc9ac9a9fda1b66ec31fa6171026efaf6da
Amar Shah <amar@shareprogress.org>
2015-09-06 09:57:33 -0400

    Use three-dot operator for range

--- 01-arrays_strings/01-unique.rb
+++ 01-arrays_strings/01-unique.rb
@@ -2,7 +2,7 @@ module Quadratic
   def self.unique_characters? string
     characters = string.chars
     characters.each_with_index do |c, i|
-      return false if (0..i - 1).any? { |j| characters[j] == c }
+      return false if (0...i).any? { |j| characters[j] == c }
     end
     true
   end
