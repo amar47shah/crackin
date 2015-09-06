1ef65ef50bb725ff5420289cd501bb8fa73ea490
Amar Shah <amar@shareprogress.org>
2015-09-06 09:46:25 -0400

    Solve Chapter 1, Question 1

---
+++ 01-arrays_strings/01-unique.rb
@@ -0,0 +1,21 @@
+module Quadratic
+  def self.unique_characters? string
+    characters = string.chars
+    characters.each_with_index do |c, i|
+      return false if (0..i - 1).any? { |j| characters[j] == c }
+    end
+    true
+  end
+end
+
+module Linear
+  def self.unique_characters? string
+    characters = string.chars
+    h = {}
+    characters.each do |c|
+      return false if h[c]
+      h[c] = true
+    end
+    true
+  end
+end
