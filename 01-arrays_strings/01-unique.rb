5c5a4763cb9b89882edb8760fcec088c44af2d2d
Amar Shah <amar@shareprogress.org>
2015-09-06 15:20:40 -0400

    Implement O(n * log n) solution for 1.1

--- 01-arrays_strings/01-unique.rb
+++ 01-arrays_strings/01-unique.rb
@@ -8,6 +8,16 @@ module Quadratic
   end
 end
 
+module Linearithmic
+  def self.unique_characters? string
+    characters = string.chars.sort
+    characters.each_with_index do |c, i|
+      return false if characters.binary_search c, max: i - 1
+    end
+    true
+  end
+end
+
 module Linear
   def self.unique_characters? string
     characters = string.chars
@@ -17,5 +27,18 @@ module Linear
       h[c] = true
     end
     true
+  end
+end
+
+class Array
+  # requires array to be pre-sorted in ascending order
+  def binary_search value, min: 0, max: size - 1
+    return false if min > max
+    midpoint = (min + max) / 2
+    case value <=> self[midpoint]
+    when -1 then binary_search value, min: min, max: midpoint - 1
+    when  0 then true
+    when  1 then binary_search value, min: midpoint + 1, max: max
+    end
   end
 end
