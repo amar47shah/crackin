c8edc9110be2b84aea1f88be55f3833007562b8f
Amar Shah <amar@shareprogress.org>
2015-09-07 19:44:28 -0400

    Use idiomatic Ruby

--- 01-arrays_strings/02-unique.rb
+++ 01-arrays_strings/02-unique.rb
@@ -1,32 +1,24 @@
 module Quadratic
   def self.unique_characters? string
     characters = string.chars
-    characters.each_with_index do |c, i|
-      return false if (0...i).any? { |j| characters[j] == c }
+    characters.each_with_index.all? do |c, i|
+      not (0...i).any? { |j| characters[j] == c }
     end
-    true
   end
 end
 
 module Linearithmic
   def self.unique_characters? string
     characters = string.chars.sort
-    characters.each_with_index do |c, i|
-      return false if characters.binary_search c, max: i - 1
+    characters.each_with_index.all? do |c, i|
+      not characters.binary_search c, max: i - 1
     end
-    true
   end
 end
 
 module Linear
   def self.unique_characters? string
-    characters = string.chars
-    h = {}
-    characters.each do |c|
-      return false if h[c]
-      h[c] = true
-    end
-    true
+    string.each_char.with_object({}).all? { |c, h| !h[c] and h[c] = true }
   end
 end
 
