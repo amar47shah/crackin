0c97ab92545c8a091bdab56cd0c432da57ec8c40
Amar Shah <amar@shareprogress.org>
2015-09-08 15:05:54 -0400

    Refactor tests

--- test/chapter_01/solution_01_test.rb
+++ test/chapter_01/solution_01_test.rb
@@ -2,87 +2,98 @@ require_relative '../test_helper'
 require_relative '../../lib/chapter_01/solution_01'
 
 module Chapter01Tests
-  class Solution01Test < Minitest::Test
-    def test_quadratic_solution
-      self.runtime = 'Quadratic'
-      test_all
+  module Solution01
+    module Tests
+      def test_empty_has_unique_characters
+        assert unique_characters? ''
+      end
+
+      def test_one_character_is_unique
+        assert unique_characters? '1'
+      end
+
+      def test_two_different_characters_are_unique
+        assert unique_characters? '12'
+      end
+
+      def test_two_of_the_same_are_not_unique
+        refute unique_characters? '11'
+      end
+
+      def test_three_characters_in_palindrome_are_not_unique
+        refute unique_characters? '121'
+      end
+
+      private
+
+      def unique_characters? string
+        namespace.unique_characters? string
+      end
+
+      def namespace
+        Chapter01::Solution01.const_get runtime
+      end
+
+      def runtime
+        fail NotImplementedError
+      end
     end
 
-    def test_linearithmic_solution
-      self.runtime = 'Linearithmic'
-      test_all
+    class QuadraticTest < Minitest::Test
+      include Tests
+
+      private
+
+      def runtime
+        'Quadratic'
+      end
     end
 
-    def test_linear_solution
-      self.runtime = 'Linear'
-      test_all
+    class LinearithmicTest < Minitest::Test
+      include Tests
+
+      private
+
+      def runtime
+        'Linearithmic'
+      end
     end
 
-    private
+    class LinearTest < Minitest::Test
+      include Tests
 
-    attr_reader :runtime
+      private
 
-    def unique_characters? string
-      runtime.unique_characters? string
+      def runtime
+        'Linear'
+      end
     end
 
-    def runtime= module_name
-      @runtime = Chapter01::Solution01.const_get module_name
-    end
+    class ArrayTest < Minitest::Test
+      def test_twenty_searches
+        20.times { test_same_as_include }
+      end
 
-    def test_all
-      test_empty_has_unique_characters
-      test_one_character_is_unique
-      test_two_different_characters_are_unique
-      test_two_of_the_same_are_not_unique
-      test_three_characters_in_palindrome_are_not_unique
-    end
+      private
 
-    def test_empty_has_unique_characters
-      assert unique_characters? ''
-    end
+      def multiples_of_five_in_order_up_to_a_hundred
+        [*(0..100).step(5)]
+      end
 
-    def test_one_character_is_unique
-      assert unique_characters? '1'
-    end
+      def presorted
+        multiples_of_five_in_order_up_to_a_hundred
+      end
 
-    def test_two_different_characters_are_unique
-      assert unique_characters? '12'
-    end
+      def random_up_to_a_hundred
+        (rand * 101).floor
+      end
 
-    def test_two_of_the_same_are_not_unique
-      refute unique_characters? '11'
-    end
-
-    def test_three_characters_in_palindrome_are_not_unique
-      refute unique_characters? '121'
-    end
-  end
-
-  class ArrayTest < Minitest::Test
-    def test_twenty_searches
-      20.times { test_same_as_include }
-    end
-
-    private
-
-    def multiples_of_five_in_order_up_to_a_hundred
-      [*(0..100).step(5)]
-    end
-
-    def presorted
-      multiples_of_five_in_order_up_to_a_hundred
-    end
-
-    def random_up_to_a_hundred
-      (rand * 101).floor
-    end
-
-    def test_same_as_include
-      r = random_up_to_a_hundred
-      expected = presorted.include? r
-      actual   = presorted.binary_search? r
-      assert_equal expected, actual
+      def test_same_as_include
+        r = random_up_to_a_hundred
+        expected = presorted.include? r
+        actual   = presorted.binary_search? r
+        assert_equal expected, actual
+      end
     end
   end
 end
