3239adfdb4d2855dd1b2b7c6671ff0b9d27ac1b5
Amar Shah <amar@shareprogress.org>
2015-09-08 23:15:14 -0400

    Use String#each_char for efficiency
    Counting distinct characters in 10,000,000 character string:
    
                           user     system      total        real
    String#chars       4.200000   0.250000   4.450000 (  4.460661)
    String#each_char   3.830000   0.180000   4.010000 (  4.032938)

--- lib/chapter_01/solution_04.rb
+++ lib/chapter_01/solution_04.rb
@@ -6,6 +6,8 @@ end
 
 module Chapter01::Solution04::Linear
   def self.palindrome_permutation? string
-    string.chars.reduce({}) { |h, c| h[c] = !h[c] ; h }.count { |_, v| v } < 2
+    string.each_char
+          .reduce({}) { |h, c| h[c] = !h[c]; h }
+          .count { |_, v| v } < 2
   end
 end
