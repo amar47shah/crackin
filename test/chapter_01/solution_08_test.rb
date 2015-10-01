f83fae59eef272fd24f98233273ab64adf0c3949
Amar Shah <amar47shah@gmail.com>
2015-10-01 11:44:02 -0400

    Add tests for 1.8

---
+++ test/chapter_01/solution_08_test.rb
@@ -0,0 +1,82 @@
+require_relative '../test_helper'
+require_relative '../support/namespace_configurable'
+require_relative '../../lib/chapter_01/solution_08'
+
+module Chapter01Tests
+  module Solution08
+    module Tests
+      def test_flowed_1x1
+        assert_flowed [[5]], [[5]]
+      end
+
+      def test_flowed_2x2
+        assert_flowed [[0, 0], [0, 4]], [[0, 2], [3, 4]]
+      end
+
+      def test_flowed_2x2_no_change
+        assert_flowed [[1, 2], [3, 4]], [[1, 2], [3, 4]]
+      end
+
+      def test_flowed_2x3
+        assert_flowed [[0, 2, 3], [0, 0, 0]], [[1, 2, 3], [0, 5, 6]]
+      end
+
+      def test_flowed_3x2
+        assert_flowed [[1, 0], [0, 0], [5, 0]], [[1, 2], [3, 0], [5, 6]]
+      end
+
+      def test_flowed_3x3
+        assert_flowed [[1, 0, 3], [0, 0, 0], [7, 0, 9]],
+                      [[1, 2, 3], [4, 0, 6], [7, 8, 9]]
+      end
+
+      def test_flowed_8x5
+        assert_flowed(
+          [[0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0]],
+          [[0, 1, 1, 0, 1],
+           [1, 1, 1, 1, 1],
+           [1, 1, 1, 0, 0],
+           [1, 1, 1, 1, 1],
+           [0, 1, 1, 1, 1],
+           [0, 1, 1, 1, 0],
+           [1, 1, 1, 1, 1],
+           [0, 1, 0, 0, 0]]
+        )
+      end
+
+      private
+
+      def assert_flowed expected, input
+        actual = flow_zeroes input
+        assert_equal expected, actual
+      end
+
+      def problem
+        Chapter01::Solution08
+      end
+
+      def flow_zeroes matrix
+        namespace.flow_zeroes matrix
+      end
+    end
+
+    class Version01Test < NamespaceConfigurable::Test
+      include Tests
+    end
+
+    class Version02Test < NamespaceConfigurable::Test
+      include Tests
+    end
+
+    class Version03Test < NamespaceConfigurable::Test
+      include Tests
+    end
+  end
+end
