e8600bc43c8a5816319bf73d0eae3b6714fde38e
Amar Shah <amar47shah@gmail.com>
2015-11-30 22:57:21 -0500

    Implement non-memoized solution to 2.1

--- lib/chapter_02/solution_01.rb
+++ lib/chapter_02/solution_01.rb
@@ -3,6 +3,7 @@ require_relative '../structure/linked_list'
 module Chapter02
   module Solution01
     module Linear; end
+    module Quadratic; end
   end
 end
 
@@ -16,6 +17,25 @@ module Chapter02::Solution01::Linear
         next snip_after previous if found[current.data]
         found[current.data] = true
         previous = current
+      end
+      self
+    end
+  end
+end
+
+module Chapter02::Solution01::Quadratic
+  class UniqueList < Structure::LinkedList
+    def remove_duplicates
+      return self if empty?
+      start = @head
+      while start
+        value = start.data
+        previous = start
+        while current = previous.next
+          next snip_after previous if current.data == value
+          previous = current
+        end
+        start = start.next
       end
       self
     end
--- test/chapter_02/solution_01_test.rb
+++ test/chapter_02/solution_01_test.rb
@@ -3,57 +3,79 @@ require_relative '../../lib/chapter_02/solution_01'
 
 module Chapter02Tests
   module Solution01
+    module Tests
+      def test_empty
+        assert_values
+      end
+
+      def test_double
+        2.times { list.push :value }
+        assert_values :value
+      end
+
+      def test_triple
+        3.times { list.push :value }
+        assert_values :value
+      end
+
+      def test_first_and_last
+        %i(same different same).each { |v| list.push v }
+        assert_values :same, :different
+      end
+
+      def test_two_each
+        %i(one one two two).each { |v| list.push v }
+        assert_values :two, :one
+      end
+
+      def test_two_alternating
+        %i(one two one two).each { |v| list.push v }
+        assert_values :two, :one
+      end
+
+      def test_two_each_of_three
+        %i(one two three two one three).each { |v| list.push v }
+        assert_values :three, :one, :two
+      end
+
+      private
+
+      def array_from list
+        array = []
+        list.each { |value| array << value }
+        array
+      end
+
+      def assert_values *expected
+        actual = array_from list.remove_duplicates
+        assert_equal expected, actual
+      end
+
+      def list
+        @list ||= klass.new
+      end
+    end
+
     module Linear
       class UniqueListTest < Minitest::Test
-        def test_empty
-          assert_values
-        end
-
-        def test_double
-          2.times { list.push :value }
-          assert_values :value
-        end
-
-        def test_triple
-          3.times { list.push :value }
-          assert_values :value
-        end
-
-        def test_first_and_last
-          %i(same different same).each { |v| list.push v }
-          assert_values :same, :different
-        end
-
-        def test_two_each
-          %i(one one two two).each { |v| list.push v }
-          assert_values :two, :one
-        end
-
-        def test_two_alternating
-          %i(one two one two).each { |v| list.push v }
-          assert_values :two, :one
-        end
-
-        def test_two_each_of_three
-          %i(one two three two one three).each { |v| list.push v }
-          assert_values :three, :one, :two
-        end
+        include Tests
 
         private
 
-        def array_from list
-          array = []
-          list.each { |value| array << value }
-          array
+        def klass
+          Chapter02::Solution01::Linear::UniqueList
         end
+      end
+    end
 
-        def assert_values *expected
-          actual = array_from list.remove_duplicates
-          assert_equal expected, actual
-        end
+    module Quadratic
+      class UniqueListTest < Minitest::Test
+        include Tests
 
-        def list
-          @list ||= Chapter02::Solution01::Linear::UniqueList.new
+        private
+
+        def klass
+          Chapter02::Solution01::Quadratic::UniqueList
         end
       end
     end
